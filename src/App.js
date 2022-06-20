f3872de8ad3e3a0827369c71dee89bcfd0db53f7
khaled saoud <khaledsaoud237@gmail.com>
2022-06-20 18:43:40 +0200

    updated the End component

--- src/App.js
+++ src/App.js
@@ -96,6 +96,7 @@ const App = () => {
           onReset={resetClickHandler}
           onAnswersCheck={() => setShowModal(true)}
           time={time}
+          username={username}
         />
       )}
 
--- src/components/End.js
+++ src/components/End.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import { formatTime } from "../utils";
 
-const End = ({ results, data, onReset, onAnswersCheck, time }) => {
+const End = ({ results, data, onReset, onAnswersCheck, time, username }) => {
   const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [isShown, setIsShown] = useState(true);
 
   useEffect(() => {
     let correct = 0;
@@ -15,6 +17,23 @@ const End = ({ results, data, onReset, onAnswersCheck, time }) => {
     setCorrectAnswers(correct);
     // eslint-disable-next-line
   }, []);
+
+  useEffect(() => {
+    const getResult = async () => {
+      const res = await axios.get("http://localhost:4000/api/results");
+      console.log(res.data);
+    };
+    getResult();
+  }, []);
+
+  const onSubmitAnswers = async () => {
+    const res = await axios.post("http://localhost:4000/api/results/result", {
+      username,
+      result: correctAnswers,
+    });
+    console.log(res.data);
+    setIsShown(false);
+  };
 
   return (
     <div className="card">
@@ -36,6 +55,13 @@ const End = ({ results, data, onReset, onAnswersCheck, time }) => {
           <button className="button is-success" onClick={onReset}>
             Try again
           </button>
+          {isShown ? (
+            <button className="button is-success" onClick={onSubmitAnswers}>
+              Submit the results
+            </button>
+          ) : (
+            <p>Data Submitted</p>
+          )}
         </div>
       </div>
     </div>
